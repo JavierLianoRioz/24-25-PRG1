5902451fa15718fd2a0fd29cef2af53b30d53d37
Midas@uneatlantico <javier.liano@alumnos.uneatlantico.es>
2024-12-11 12:12:36 +0100

    Matrices - a1.0
    hemos hecho el codigo base.
    - [x] hacer una matríz con sentido.
    - [ ] hacer un parse.
    - [ ] configurar el movimiento.

--- entregas/lianoJavier/src/013 Matrices/Matrices.java
+++ entregas/lianoJavier/src/013 Matrices/Matrices.java
@@ -1,56 +1,34 @@
-import java.util.Scanner;
-
 class Matrices {
   public static void main(String[] args) {
-
     int[][] mapa = {
-        { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
-        { 0, 1, 0, 0, 1, 1, 1, 0, 1, 1 },
-        { 0, 1, 0, 0, 1, 0, 1, 0, 1, 0 },
-        { 0, 1, 1, 1, 1, 0, 1, 0, 1, 0 },
-        { 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 },
-        { 0, 0, 0, 1, 1, 1, 1, 0, 1, 0 },
-        { 0, 0, 0, 1, 0, 0, 0, 0, 1, 0 },
-        { 0, 1, 1, 1, 0, 1, 1, 1, 1, 0 },
-        { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
-        { 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0,0,0,0}
     };
 
-    int[] posicion = { 1, 0 };
-
-    int[] posicion = mover();
-
-    imprimir(mapa, posicion);
+    dibujar(mapa);
   }
 
-  static String parse(int pixel) {
-    final String[] SKINS = {
-        "[ ]",
-        "///",
-        "[-]",
-        "_O_"
-    };
-    return SKINS[pixel];
-    // TODO: Añadir parsers distintos.
-  }
-
-  static void imprimir(int[][] mapa, int[] coordenadaPersonaje) {
-    int PERSONAJE = 3;
-    boolean isPersonaje;
-    String skin;
-
-    for (int fila = 0; fila < mapa.length; fila++) {
-      for (int columna = 0; columna < mapa[columna].length; columna++) {
-        skin = parse(mapa[fila][columna]);
-
-        isPersonaje = fila == coordenadaPersonaje[1] && columna == coordenadaPersonaje[0];
-        skin = isPersonaje ? parse(PERSONAJE) : skin;
-
-        System.out.print(skin);
+  private static void dibujar(int[][] mundo) {
+    String pixel;
+    final String BORDE = "=";
+    System.out.println(BORDE.repeat(mundo[0].length));
+    for (int row = 0; row < mundo.length; row++) {
+      for (int column = 0; column < mundo[row].length; column++) {
+        pixel = ""+mundo[row][column];
+        System.out.print(pixel);
       }
-
       System.out.println();
     }
+    System.out.println(BORDE.repeat(mundo[mundo.length - 1].length));
   }
 }
