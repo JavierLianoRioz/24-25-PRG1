b488c23ed7dc25bde51279db12a4fb6b2e6a79fc
Midas@tower <javier.liano@alumnos.uneatlantico.es>
2024-12-11 22:50:42 +0100

    Matrices - a1.3
    hemos hecho el codigo base.
    - [x] hacer una matríz con sentido.
    - [x] hacer un parse.
    - [x] configurar el movimiento.
    - [x] configurar salida.
    ---
    arreglamos errores de código

--- entregas/lianoJavier/src/013 Matrices/Matrices.java
+++ entregas/lianoJavier/src/013 Matrices/Matrices.java
@@ -3,31 +3,36 @@ import java.util.Scanner;
 class Matrices {
   public static void main(String[] args) {
     int[][] mapa = getMapa(0);
-    int[] coordenadasJugador = getCoordenadas(3,2);
+    int[] coordenadasJugador = getCoordenadas(3, 2);
 
     boolean isPlaying = true;
-    do{
+    do {
       dibujar(mapa, coordenadasJugador);
-      isPlaying = jugabilidad();
+      isPlaying = jugabilidad(coordenadasJugador);
     } while (isPlaying);
   }
 
-  static boolean jugabilidad() {
+  static boolean jugabilidad(int[] coordenadas) {
     String entrada = new Scanner(System.in).nextLine();
     move(coordenadasJugador, entrada);
     return salir(entrada);
   }
 
-  static boolean salir(Scanner entrada) {
-    if (movimiento.equalsIgnoreCase("ñ")) return false;
+  static boolean salir(String entrada) {
+    if (entrada.equalsIgnoreCase("ñ"))
+      return false;
     return true;
   }
 
   static void move(int[] coordenadas, String movimiento) {
-    if (movimiento.equalsIgnoreCase("a")) coordenadas[0] = coordenadas[0] - 1;
-    if (movimiento.equalsIgnoreCase("d")) coordenadas[0] = coordenadas[0] + 1;
-    if (movimiento.equalsIgnoreCase("w")) coordenadas[1] = coordenadas[1] - 1;
-    if (movimiento.equalsIgnoreCase("a")) coordenadas[1] = coordenadas[1] + 1;
+    if (movimiento.equalsIgnoreCase("a"))
+      coordenadas[0] = coordenadas[0] - 1;
+    if (movimiento.equalsIgnoreCase("d"))
+      coordenadas[0] = coordenadas[0] + 1;
+    if (movimiento.equalsIgnoreCase("w"))
+      coordenadas[1] = coordenadas[1] - 1;
+    if (movimiento.equalsIgnoreCase("a"))
+      coordenadas[1] = coordenadas[1] + 1;
   }
 
   static int getLongitudMaxima(int[][] matriz) {
@@ -42,33 +47,33 @@ class Matrices {
 
   static int[][] getMapa(int index) {
     final int[][][] MUNDOS = {
-    {
-      { 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 9, 9, 9, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
-      { 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 3, 3, 3, 3, 3, 3, 3, 3 },
-      { 7, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 7, 3, 3, 3, 3 },
-      { 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 2, 2, 3, 3 },
-      { 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 7, 7, 2, 2, 3 },
-      { 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 8, 7, 7, 2, 3 },
-      { 5, 5, 5, 5, 5, 0, 2, 0, 5, 5, 5, 5, 5, 0, 0, 0, 2, 0, 0, 0, 0, 5, 7, 2, 2 },
-      { 5, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 0, 0, 0, 0, 8, 7, 7, 2 },
-      { 5, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 0, 0, 0, 0, 0, 5, 7, 7 },
-      { 5, 0, 1, 1, 1, 0, 2, 0, 1, 1, 1, 0, 5, 8, 8, 8, 2, 8, 8, 8, 8, 5, 5, 7, 7 },
-      { 5, 0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0, 5, 8, 6, 6, 2, 6, 6, 6, 5, 5, 5, 7, 7 },
-      { 5, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 5, 6, 6, 6, 2, 6, 5, 5, 5, 5, 5, 7, 7 },
-      { 5, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 5, 6, 6, 6, 2, 5, 5, 5, 8, 8, 8, 5, 7 },
-      { 5, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 5, 6, 6, 5, 2, 5, 6, 6, 6, 8, 8, 5, 7 },
-      { 5, 0, 1, 1, 9, 1, 1, 1, 9, 1, 1, 0, 5, 6, 5, 5, 2, 6, 6, 8, 8, 8, 8, 8, 7 },
-      { 5, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 5, 5, 5, 6, 2, 6, 6, 6, 8, 1, 1, 1, 1 },
-      { 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 4, 4, 6, 2, 2, 2, 2, 2, 9, 0, 0, 0 },
-      { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 6, 6, 8, 8, 8, 8, 1, 0, 0, 0 },
-      { 7, 7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 6, 6, 8, 8, 8, 8, 1, 0, 5, 0 }
-      }
+        {
+            { 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 9, 9, 9, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 3, 3, 3, 3, 3, 3, 3, 3 },
+            { 7, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 7, 3, 3, 3, 3 },
+            { 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 2, 2, 3, 3 },
+            { 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 7, 7, 2, 2, 3 },
+            { 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 8, 7, 7, 2, 3 },
+            { 5, 5, 5, 5, 5, 0, 2, 0, 5, 5, 5, 5, 5, 0, 0, 0, 2, 0, 0, 0, 0, 5, 7, 2, 2 },
+            { 5, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 0, 0, 0, 0, 8, 7, 7, 2 },
+            { 5, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 0, 0, 0, 0, 0, 5, 7, 7 },
+            { 5, 0, 1, 1, 1, 0, 2, 0, 1, 1, 1, 0, 5, 8, 8, 8, 2, 8, 8, 8, 8, 5, 5, 7, 7 },
+            { 5, 0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0, 5, 8, 6, 6, 2, 6, 6, 6, 5, 5, 5, 7, 7 },
+            { 5, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 5, 6, 6, 6, 2, 6, 5, 5, 5, 5, 5, 7, 7 },
+            { 5, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 5, 6, 6, 6, 2, 5, 5, 5, 8, 8, 8, 5, 7 },
+            { 5, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 5, 6, 6, 5, 2, 5, 6, 6, 6, 8, 8, 5, 7 },
+            { 5, 0, 1, 1, 9, 1, 1, 1, 9, 1, 1, 0, 5, 6, 5, 5, 2, 6, 6, 8, 8, 8, 8, 8, 7 },
+            { 5, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 5, 5, 5, 6, 2, 6, 6, 6, 8, 1, 1, 1, 1 },
+            { 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 4, 4, 6, 2, 2, 2, 2, 2, 9, 0, 0, 0 },
+            { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 6, 6, 8, 8, 8, 8, 1, 0, 0, 0 },
+            { 7, 7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 6, 6, 8, 8, 8, 8, 1, 0, 5, 0 }
+        }
     };
     return MUNDOS[index];
   }
 
   static int[] getCoordenadas(int y, int x) {
-    int[] coordenadas = {y,x};
+    int[] coordenadas = { y, x };
     return coordenadas;
   }
 
